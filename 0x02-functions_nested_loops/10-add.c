05fae2598d1a1b55bc3aeacc78bb6849fbe01ac6
DevanMasoleDimo <shimmydimo14@gmail.com>
2023-07-19 11:28:51 -0700

    5 to 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * add - Adds two values
+ * @n1: The first number to be computed
+ * @n2: The first number to be computed
+ * Return: Interger value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,7 +14,7 @@ int _abs(int);
 
 int print_last_digit(int);
 
-void jack_baucer(void);
+void jack_bauer(void);
 
 void times_table(void);
 
