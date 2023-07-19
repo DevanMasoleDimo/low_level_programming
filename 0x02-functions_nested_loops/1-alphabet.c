bd3aad3f7059eba94dd66b96964b915a88975b40
DevanMasoleDimo <shimmydimo14@gmail.com>
2023-07-19 02:58:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * ptint_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'n'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
